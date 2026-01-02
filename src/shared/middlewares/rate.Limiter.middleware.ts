e9b637f58c52c96f0803418ad6ae7ca85e3713b0
Boris Espinosa <borisem.dev@gmail.com>
2026-01-02 15:53:08 -0300

    standarized rate limiting responses to match response format

--- src/shared/middlewares/rate.Limiter.middleware.ts
+++ src/shared/middlewares/rate.Limiter.middleware.ts
@@ -6,23 +6,37 @@ const isTestEnv = process.env.NODE_ENV === 'test';
 export const authLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   max: 10,
-  message: 'Demasiados intentos de login, intenta de nuevo en 5 minutos',
+  message: {
+    status: 429,
+    message: 'Demasiados intentos de login, intenta de nuevo en 5 minutos',
+  },
   skipSuccessfulRequests: true,
   skip: () => isTestEnv,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 export const createLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 10,
-  message: 'Demasiadas creaciones, intenta de nuevo en 1 minuto',
+  message: {
+    status: 429,
+    message: 'Demasiadas creaciones, intenta de nuevo en 1 minuto',
+  },
   skip: () => isTestEnv,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 export const normalLimiter = [
   rateLimit({
     windowMs: 1000,
     max: 2,
-    message: 'Demasiadas peticiones desde esta IP, intenta de nuevo más tarde',
+    message: {
+      status: 429,
+      message:
+        'Demasiadas peticiones desde esta IP, intenta de nuevo más tarde',
+    },
     standardHeaders: true,
     legacyHeaders: false,
     skip: () => isTestEnv,
@@ -30,7 +44,11 @@ export const normalLimiter = [
   rateLimit({
     windowMs: 60 * 1000,
     max: 30,
-    message: 'Demasiadas peticiones desde esta IP, intenta de nuevo más tarde',
+    message: {
+      status: 429,
+      message:
+        'Demasiadas peticiones desde esta IP, intenta de nuevo más tarde',
+    },
     standardHeaders: true,
     legacyHeaders: false,
     skip: () => isTestEnv,
